90c0ba1caa9ddb5e1b89c9a7e9940fb03a6cac3e
Kissa1001 <kissa101295@gmail.com>
2016-01-15 23:29:50 -0500

    final touch

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,28 @@
+
 $(document).ready(function(){
+
     $('#button').click(function(){
-    	var Add=$('input[name=textarea]').val(); 
-    	$('.list').append('<div class="item">' + Add + '</div>');
+    	var Add=$('input[name=textarea]').val();
+        $('input[name=textarea]').val(' '); 
+    	$('.list').append('<div class="item">'+ '<i class="fa fa-check"></i>' + Add +'<i class="fa fa-times"></i>'+'</div>');
     });
-    $(document).on('click','.item',function(){
-        $(this).fadeOut(500);
+
+    $('input[name=textarea]').keydown(function(e) {
+       if(e.which == 13) {
+        $('#button').click();
+        return false;
+       }
+    });
+
+    $(document).on('click','.fa-times',function(){
+        $(this).parent().fadeOut(500);
+    });
+
+    $(document).on('click','.fa-check', function(){
+        $(this).parent().css("text-decoration","line-through");
+    }); 
+
+    $(document).on('click','.fa-shopping-cart',function(){
+        location.reload();
     });
 });
