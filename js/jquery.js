f76abe55c11994d4493eebd896780503b40ad02b
Thu Linh Nguyen <kissa101295@gmail.com>
2016-01-24 21:40:23 -0500

    OOP update

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,32 @@
-$(document).ready(function(){
-    $('#button').click(function(){
-    	var Add=$('input[name=textarea]').val(); 
-    	$('.list').append('<div class="item">' + Add + '</div>');
+
+$(function(){
+
+    $('#shopping-cart').submit(function(event){
+        event.preventDefault();
+        addItem();
     });
-    $(document).on('click','.item',function(){
-        $(this).fadeOut(500);
+
+    $('#addItem').click(function(){  
+        addItem();
     });
+
+    var addItem = function(){
+        var item = $('#add').val();
+        $('#add').val(' ');
+
+        $('ul.list').append('<li><i class="fa fa-check"></i>' + item + '<i class="fa fa-times"></i></li>');
+
+        $('.fa-times').on('click',function(){
+            $(this).parent().fadeOut(500);
+        });
+
+        $('.fa-check').on('click', function(){
+            $(this).parent().css("text-decoration","line-through");
+        }); 
+
+        $('.fa-shopping-cart').on('click',function(){
+            location.reload();
+        });
+    };
+
 });
