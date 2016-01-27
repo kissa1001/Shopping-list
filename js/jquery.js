1299eddbc320b27b30a931a051e96ba999ae1af2
Kissa1001 <kissa101295@gmail.com>
2016-01-26 22:13:01 -0500

    OOP

--- js/jquery.js
+++ js/jquery.js
@@ -1,38 +1,50 @@
+var Item = function(description){
+    this.description = description;
+};
+
+Item.prototype.show = function() {
+    return '<li><i class="fa fa-check"></i>' + this.description + '<i class="fa fa-times"></i></li>';
+};
+
+var ShoppingList = function(selector, button, items, list){
+    this.form = $(selector);
+    this.button= $(button);
+    this.list = $(list);
+    this.items = items;
+};
+
+ShoppingList.prototype.start = function(){
+    this.form.submit(this.formSubmitHandler.bind(this));
+    
+    this.list.on('click', '.fa-times', {shoppingList: this}, function(){
+        //shoppingList.items
+        $(this).parent().fadeOut(500);
+    });
+    
+    this.list.on('click', '.fa-check', function(){
+        $(this).parent().css("text-decoration","line-through");
+    });
+    $('.fa-shopping-cart').on('click', {ShoppingList: this}, function(){
+        ShoppingList.list.empty();
+    });
+};
+
+ShoppingList.prototype.addItem =function(item){
+    this.items.push(item);
+    this.list.append(item.show());
+};
+
+ShoppingList.prototype.formSubmitHandler = function(event) {
+    event.preventDefault();
+    var item = new Item(this.button.val());
+    this.button.val('');
+    this.addItem(item);
+};
 
 $(function(){
-
-    $('#shopping-cart').submit(function(event){
-        event.preventDefault();
-        addItem();
-    });
-
-    $('#addItem').click(function(){  
-        addItem();
-    });
-
-    var addItem = function(){
-        var item = $('#add').val();
-        $('#add').val(' ');
-
-        $('ul.list').append('<li><i class="fa fa-check"></i>' + item + '<i class="fa fa-times"></i></li>');
-
-        $('.fa-times').on('click',function(){
-            $(this).parent().fadeOut(500);
-        });
-
-        $('.fa-check').on('click', function(){
-            $(this).parent().css("text-decoration","line-through");
-        }); 
-
-        $('.fa-shopping-cart').on('click',function(){
-            location.reload();
-        });
-    };
-
+    var shoppingList = new ShoppingList('#shopping-cart', '#add', [], '.list');
+    shoppingList.start();
 });
-    
-
-    
 
 
   
