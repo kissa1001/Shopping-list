dfe3fc66d22abde08664b315df7662307fafa500
Kissa1001 <kissa101295@gmail.com>
2016-01-29 17:58:14 -0500

    shopcart colors

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,35 @@
 
 $(document).ready(function(){
 
+    var countVegie= 0;
+    var countFruit= 0;
+    
     $('#button').click(function(){
-    	var Add=$('input[name=textarea]').val();
-        $('input[name=textarea]').val(' '); 
+    	var Add=$('#addItem').val();
+        $('#addItem').val(' '); 
     	$('.list').append('<div class="item">'+ '<i class="fa fa-check"></i>' + Add +'<i class="fa fa-times"></i>'+'</div>');
+
+            
+        if(Add.match('fruit')) {
+            countFruit ++ ;
+        }
+        else if(Add.match('vegie')){
+            countVegie ++;
+        }
+
+        if(countVegie > countFruit){
+            $('.fa-shopping-cart').css('color','green');
+        }
+        else if(countVegie < countFruit){
+            $('.fa-shopping-cart').css('color','red');
+        }
+        else if(countVegie = countFruit) {
+            $('.fa-shopping-cart').css('color','orange');
+        }
+        
     });
 
-    $('input[name=textarea]').keydown(function(e) {
+    $('#addItem').keydown(function(e) {
        if(e.which == 13) {
         $('#button').click();
         return false;
@@ -25,4 +47,16 @@ $(document).ready(function(){
     $(document).on('click','.fa-shopping-cart',function(){
         location.reload();
     });
+
+
+
+        
+
+    
+
+    
+
+
+
+
 });
