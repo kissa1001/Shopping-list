5beae6d0efb06ab9ff09d1ef5947abc151c18013
Kissa1001 <kissa101295@gmail.com>
2016-02-08 16:08:57 -0500

    3rd push

--- js/jquery.js
+++ js/jquery.js
@@ -1,96 +1,94 @@
 
 $(document).ready(function(){
 
-var Item = function(description){
-    this.description = description;
-    this.bought = false;
-    this.quantity = 1;
-};
+    var Item = function(description){
+        this.description = description;
+        this.bought = false;
+        this.quantity = 1;
+    };
 
-Item.prototype.itemCount = function  (value){
+    Item.prototype.itemCount = function  (value){
         if(this.description == value){
             return true;
         }
         else{
             return false;
         }
-    return this.length;
-};
+        return this.length;
+    };
 
-Item.prototype.updateCart = function  (){
-    if(this.itemCount('vegie') > this.itemCount('fruit')){
-        $('.fa-shopping-cart').css('color','green');
-    }
-    else if (this.itemCount('vegie') < this.itemCount('fruit')){
-        $('.fa-shopping-cart').css('color','red');
-    }
-    else{
-        $('.fa-shopping-cart').css('color','orange');
-    }
-};
-
-var shoppingListItems = function(items,form,list){
-    this.form = $(form);
-    this.list = $(list);
-    this.items = items;
-}
-
-shoppingListItems.prototype.updateList = function  (){
-    this.list.empty();
-        $.each(this.items, function(index, description){
-         var template = Handlebars.compile($('#item-template').html());
-         this.list.append(template({description:description,index:index}));
-    });
-};
-
-shoppingListItems.prototype.totalBoughtItems = function () {
-    total = 0;
-    $.each(this.data, function(index, element) {
-        if (item.bought) {
-            total += item.quantity;
+    Item.prototype.updateCart = function  (){
+        if(this.itemCount('vegie') > this.itemCount('fruit')){
+            $('.fa-shopping-cart').css('color','green');
         }
-    });
-    return total;
-};
+        else if (this.itemCount('vegie') < this.itemCount('fruit')){
+            $('.fa-shopping-cart').css('color','red');
+        }
+        else{
+            $('.fa-shopping-cart').css('color','orange');
+        }
+    };
 
-shoppingListItems.prototype.addItems = function(item){
-    var item = new Item($('#addItem').val());
-    $('#addItem').val(' ');
-    this.list.append(item);
-    this.items.push({item: item, quantity: 1});
+    var shoppingListItems = function(items,form,list){
+        this.form = $(form);
+        this.list = $(list);
+        this.items = items;
+    }
+
+    shoppingListItems.prototype.updateList = function  (){
+        this.list.empty();
+        $.each(this.items, function(index, description){
+           var template = Handlebars.compile($('#item-template').html());
+           this.list.append(template({description:description,index:index}));
+       });
+    };
+
+    shoppingListItems.prototype.totalBoughtItems = function () {
+        total = 0;
+        $.each(this.data, function(index, element) {
+            if (item.bought) {
+                total += item.quantity;
+            }
+        });
+        return total;
+    };
+
+    shoppingListItems.prototype.addItems = function(item){
+        var item = new Item($('#addItem').val());
+        $('#addItem').val(' ');
+        this.list.append(item);
+        this.items.push({item: item, quantity: 1});
 
         this.list.on('click','.fa-times',function(){
-        $(this).parent().fadeOut(500, function(){
-            var index = $(this).attr('id').substr(5);
-            shoppingListItems.splice(index,1);
-            updateCart();
-            updateList();
+            $(this).parent().fadeOut(500, function(){
+                var index = $(this).attr('id').substr(5);
+                shoppingListItems.splice(index,1);
+                item.updateCart();
+                this.list.updateList();
+            });
         });
-    });
 
-    this.list.on('click','.fa-check', function(){
-        $(this).parent().css("text-decoration","line-through");
-         totalBoughtItems();
-        updateList();
-    }); 
+        this.list.on('click','.fa-check', function(){
+            $(this).parent().css("text-decoration","line-through");
+            item.totalBoughtItems();
+            this.list.updateList();
+        }); 
 
-    this.list.on('click','.fa-shopping-cart',function(){
-        shoppingListItems = [];
-        updateList();
+        this.list.on('click','.fa-shopping-cart',function(){
+            shoppingListItems = [];
+            this.list.updateList();
+            itemupdateCart();
+        });
+
+        this.updateList();
         updateCart();
-    });
-
-    this.updateList();
-    updateCart();
-
-
-};
+    };
     
-shoppingListItems.prototype.show = function(event){
-    this.form.submit(this.addItems.bind(this));
-};
+    shoppingListItems.prototype.show = function(event){
+        this.form.submit(this.addItems.bind(this));
+    };
 
-var shoppingList = new shoppingListItems( [], '#shopping-cart', '.list');
+    var shoppingList = new shoppingListItems( [], '#shopping-cart', '.list');
     shoppingList.show();
 
 
